d59394d095448f99daac7ee04ab6e7549a2fe75d
Abhinav <85792055+AbhinavTheDev@users.noreply.github.com>
2024-05-29 13:45:20 +0530

    Update process-env.d.ts

--- packages/blog-starter-kit/themes/personal/process-env.d.ts
+++ packages/blog-starter-kit/themes/personal/process-env.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
 	interface ProcessEnv {
 		[key: string]: string | undefined;
 		NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT: string;
-		NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST: "gix.hashnode.dev";
+		NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST: string;
 		// add more environment variables and their types here
 	}
 }
